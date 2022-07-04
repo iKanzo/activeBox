7fa13323fa994bea28a15de9d9a669d43903375f
iKanzo <101828330+iKanzo@users.noreply.github.com>
2022-07-04 13:49:05 +0500

    Fix

---
+++ ActiveBox/app.js
@@ -0,0 +1,64 @@
+$(function () {
+
+   /* Header fixed */
+
+   let header = $("#header");
+   let intro = $("#intro");
+   let introH = intro.innerHeight();
+   let scrollPos = $(window).scrollTop();
+   let nav = $("#nav");
+   let navToggle = $("#navToggle");
+
+   checkScroll(scrollPos, introH);
+
+
+   $(window).on("scroll resize", function () {
+      introH = intro.innerHeight();
+      scrollPos = $(this).scrollTop();
+      checkScroll(scrollPos, introH);
+   });
+
+   function checkScroll(scrollPos, introH) {
+      if (scrollPos > introH) {
+         header.addClass("fixed");
+      }
+      else {
+         header.removeClass("fixed");
+      }
+   };
+
+
+   /* Scroll */
+
+   $("[data-scroll]").on("click", function (event) {
+      event.preventDefault();
+      let blockID = $(this).data('scroll');
+      let blockOffSet = $(blockID).offset().top;
+      nav.removeClass("show");
+      $("html, body").animate({
+         scrollTop: blockOffSet - 70
+      }, 600);
+   });
+
+
+   /* Nav Toggle */
+
+   $("#navToggle").on("click", function (event) {
+      event.preventDefault();
+      nav.toggleClass("show");
+   });
+
+    /* Clients */
+   
+   let slider = $("#clientsSlider");
+
+   slider.slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade: true,
+      arrows: false,
+      dots: true
+    });
+
+});
